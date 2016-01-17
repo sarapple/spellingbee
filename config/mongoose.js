37f5e9a60baaae8ca26bd2b9af5dcbfd3e7a7fc0
Sara Wong <sara@Saras-MacBook-Air.local>
2016-01-16 20:44:10 -0800

    updated with mongodb-uri module

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,17 +1,19 @@
 // SUMMARY: Connect to the MONGODB database (generate error if can't connect). Load all files stored in the models foder
-var mongoose, env, config, fs, connect, models_path;
+var mongoose, env, config, fs, connect, models_path, uriUtil;
 
 mongoose = require('mongoose');
 env      = process.env.NODE_ENV || 'development';	//set env to whatever is set in NODE, if not set then use development
 config   = require('./config')[env];							//whatever comes back from it, set it to env
 fs       = require('fs');
+uriUtil  = require('mongodb-uri');
 
 // Bootstrap db connection
 // Connect to mongodb
 connect  = function() {
-	var options;
+	var options, mongooseUri;
 
 	options = { server : { socketOptions : { keepAlive : 1 } } };
+	mongooseUri = uriUtil.formatMongoose(config.db);
 	mongoose.connect(config.db, options);
 };
 connect();
